76f741c6ace4ca10d2cb990081c182c1a57cf777
eejdoowad <eejdoowad@gmail.com>
2018-09-05 22:03:44 -0400

    update typings, still needs a lot of work

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
 declare module 'sqorn' {
 
-
-
   /**
    * The sqorn SQL query builder
    */
@@ -30,43 +28,115 @@ declare module 'sqorn' {
     /**
      * FROM clause - specify query table
      * 
-     * Accepts a single table or a join between multiple tables.
+     * Accepts table as template string
      * 
      * @example
      * sq.frm`book`
      * // select * from book
      * sq.frm`book join comment`
      * // select * from book join comment
+     * sq.frm`$${'book'}`
+     * // select * from book
      * sq`book`
      * // select * from book
      */
-    frm(strings: string[], ...args: any[]): sq
+    frm(strings: TemplateStringsArray, ...args: any[]): sq
+
+    /**
+     * FROM clause - specify query table
+     * 
+     * Accepts array of tables names
+     * 
+     * @example
+     * sq.frm('book')
+     * // select * from book
+     * sq.frm('book', 'author', 'vote')
+     * // select * from book, author, vote
+     */
+    frm(...tables: string[]): sq
 
     /**
      * WHERE clause - specify query filters
      * 
+     * Accepts WHERE conditions as template string. Multiple calls to `.whr`
+     * are joined with `'and'`.
+     * 
      * @example
-     * sq.frm`person`.whr`age < 18`
+     * sq.frm`person`.whr`age < ${18}`
      * // select * from person where age < 18
-     * sq.del.frm`person`.whr`age < 7`
+     * sq.del.frm`person`.whr`age < ${7}`
      * // delete from person where age < 7
-     * sq`person``age < 18`
+     * sq.frm`person`.whr`age < ${7}`.set`group = ${'infant'}`
+     * // update person set group = 'infant' where age < 7
+     * sq.frm`person`.whr`age > ${3}`.whr`age < ${7}`
+     * // select * from person where age > 3 and age < 7
+     * sq`person``age < ${18}`
      * // select * from person where age < 18
      */
-    whr(strings: string[], ...args: any[]): sq
+    whr(strings: TemplateStringsArray, ...args: any[]): sq
+
+    /**
+     * WHERE clause - specify query filters
+     * 
+     * Accepts conditions as objects. Object keys are column names tested for
+     * equality against object values. Use values of type `sq` to build
+     * non-equality conditions. Keys within an object are joined with `'and'`,
+     * while objects are joined with `'or'`. Multiple calls to `.whr` are
+     * joined with `'and'`.
+     * 
+     * @example
+     * sq.frm`person`.whr({ age: 17 })
+     * // select * from person where age = 17
+     * sq.frm`person`.whr({ minAge: sq.l`age < ${17}` })
+     * // select * from person where age = 17
+     * sq.frm`person`.whr({ age: 7, gender: 'male' }, { name: 'Jo' })
+     * // select * from person where age = 7 and gender = 'male' or name = 'Jo'
+     * sq.frm`person`.whr({ age: 7 }).whr({ name: 'Joe' })
+     * // select * from person where age = 7 and name = 'Joe'
+     */
+    whr(...conditions: { [key: string]: any }[]): sq
 
     /**
      * SELECT or RETURNING clause - specify columns query returns
      * 
+     * Accepts columns as template string
+     * 
      * @example
      * sq.ret`1, 2, 3`
      * // select 1, 2, 3
-     * sq.del.frm`person`.ret`age`
-     * // delete from person returning age
-     * sq`person`()`age`
-     * // select age from person
+     * sq.frm`book.`ret`title`
+     * // select title from book
+     * sq.del.frm`person`.ret`id, age`
+     * // delete from person returning id, age
+     * sq.frm`person`.set`age = age + 1`.ret`id, age`
+     * // update person set age = age + 1 returning id, age
+     * sq.frm`person`.ins`age`.val`${12}`.ret`id, age`
+     * // insert into person (age) values (12) returning id, age
+     * sq`person``age > ${7}``id, age`
+     * // select id, age from person where age > 7
      */
-    ret(strings: string[], ...args: any[]): sq
+    ret(strings: TemplateStringsArray, ...args: any[]): sq
+
+    /**
+     * SELECT or RETURNING clause - specify columns query returns
+     * 
+     * Accepts columns as strings
+     * 
+     * @example
+     * sq.ret('1', '2', '3')
+     * // select 1, 2, 3
+     * sq.frm('book').ret('title')
+     * // select title from book
+     * sq.del.frm('person').ret('id', 'age')
+     * // delete from person returning id, age
+     * sq.frm('person').set`age = age + 1`.ret('id', 'age')
+     * // update person set age = age + 1 returning id, age
+     * sq.frm('person').ins('age').val(12).ret('id', 'age')
+     * // insert into person (age) values (12) returning id, age
+     * sq`person``age > ${7}`('id', 'age')
+     * // select id, age from person where age > 7
+     */
+    ret(...columns: string[]): sq
 
     /**
      * INSERT column - specify columns to insert using tagged template literal
@@ -81,7 +151,7 @@ declare module 'sqorn' {
      * sq`person`()`id`.ins`age`.val`23`.val`40`
      * // insert into person (age) values (23), (40) returning id
      */
-    ins(strings: string[], ...args: any[]): sq
+    ins(strings: TemplateStringsArray, ...args: any[]): sq
     /**
      * INSERT value - specify value(s) to insert as objects
      * 
@@ -108,7 +178,7 @@ declare module 'sqorn' {
      * sq`person`()`id`.ins`age`.val`23`.val`40`
      * // insert into person (age) values (23), (40) returning id
      */
-    val(strings: string[], ...args: any[]): sq
+    val(strings: TemplateStringsArray, ...args: any[]): sq
 
     /**
      * DELETE - marks the query as a delete query
