c870eed217f4b02b22b2769377aa338ddaa64252
eejdoowad <eejdoowad@gmail.com>
2018-09-07 17:29:02 -0400

    updated typings

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ declare module 'sqorn' {
     /**
      * EXPRESS query builder - shorthand query syntax
      * 
-     * First call is `.frm`. Second call is `.whr`. Third call is `.ret`.
+     * First call is `.from`. Second call is `.where`. Third call is `.return`.
      * Subsequent calls ignore.
      * 
      * @example
@@ -31,16 +31,16 @@ declare module 'sqorn' {
      * Accepts table as template string
      * 
      * @example
-     * sq.frm`book`
+     * sq.from`book`
      * // select * from book
-     * sq.frm`book join comment`
+     * sq.from`book join comment`
      * // select * from book join comment
-     * sq.frm`$${'book'}`
+     * sq.from`$${'book'}`
      * // select * from book
      * sq`book`
      * // select * from book
      */
-    frm(strings: TemplateStringsArray, ...args: any[]): sq
+    from(strings: TemplateStringsArray, ...args: any[]): sq
 
     /**
      * FROM clause - specify query table
@@ -48,32 +48,32 @@ declare module 'sqorn' {
      * Accepts array of tables names
      * 
      * @example
-     * sq.frm('book')
+     * sq.from('book')
      * // select * from book
-     * sq.frm('book', 'author', 'vote')
+     * sq.from('book', 'author', 'vote')
      * // select * from book, author, vote
      */
-    frm(...tables: string[]): sq
+    from(...tables: string[]): sq
 
     /**
      * WHERE clause - specify query filters
      * 
-     * Accepts WHERE conditions as template string. Multiple calls to `.whr`
+     * Accepts WHERE conditions as template string. Multiple calls to `.where`
      * are joined with `'and'`.
      * 
      * @example
-     * sq.frm`person`.whr`age < ${18}`
+     * sq.from`person`.where`age < ${18}`
      * // select * from person where age < 18
-     * sq.del.frm`person`.whr`age < ${7}`
+     * sq.delete.from`person`.where`age < ${7}`
      * // delete from person where age < 7
-     * sq.frm`person`.whr`age < ${7}`.set`group = ${'infant'}`
+     * sq.from`person`.where`age < ${7}`.set`group = ${'infant'}`
      * // update person set group = 'infant' where age < 7
-     * sq.frm`person`.whr`age > ${3}`.whr`age < ${7}`
+     * sq.from`person`.where`age > ${3}`.where`age < ${7}`
      * // select * from person where age > 3 and age < 7
      * sq`person``age < ${18}`
      * // select * from person where age < 18
      */
-    whr(strings: TemplateStringsArray, ...args: any[]): sq
+    where(strings: TemplateStringsArray, ...args: any[]): sq
 
     /**
      * WHERE clause - specify query filters
@@ -81,20 +81,20 @@ declare module 'sqorn' {
      * Accepts conditions as objects. Object keys are column names tested for
      * equality against object values. Use values of type `sq` to build
      * non-equality conditions. Keys within an object are joined with `'and'`,
-     * while objects are joined with `'or'`. Multiple calls to `.whr` are
+     * while objects are joined with `'or'`. Multiple calls to `.where` are
      * joined with `'and'`.
      * 
      * @example
-     * sq.frm`person`.whr({ age: 17 })
+     * sq.from`person`.where({ age: 17 })
      * // select * from person where age = 17
-     * sq.frm`person`.whr({ minAge: sq.l`age < ${17}` })
+     * sq.from`person`.where({ minAge: sq.l`age < ${17}` })
      * // select * from person where age = 17
-     * sq.frm`person`.whr({ age: 7, gender: 'male' }, { name: 'Jo' })
+     * sq.from`person`.where({ age: 7, gender: 'male' }, { name: 'Jo' })
      * // select * from person where age = 7 and gender = 'male' or name = 'Jo'
-     * sq.frm`person`.whr({ age: 7 }).whr({ name: 'Joe' })
+     * sq.from`person`.where({ age: 7 }).where({ name: 'Joe' })
      * // select * from person where age = 7 and name = 'Joe'
      */
-    whr(...conditions: { [key: string]: any }[]): sq
+    where(...conditions: { [key: string]: any }[]): sq
 
     /**
      * SELECT or RETURNING clause - specify columns query returns
@@ -102,20 +102,20 @@ declare module 'sqorn' {
      * Accepts columns as template string
      * 
      * @example
-     * sq.ret`1, 2, 3`
+     * sq.return`1, 2, 3`
      * // select 1, 2, 3
-     * sq.frm`book.`ret`title`
+     * sq.from`book.`ret`title`
      * // select title from book
-     * sq.del.frm`person`.ret`id, age`
+     * sq.delete.from`person`.return`id, age`
      * // delete from person returning id, age
-     * sq.frm`person`.set`age = age + 1`.ret`id, age`
+     * sq.from`person`.set`age = age + 1`.return`id, age`
      * // update person set age = age + 1 returning id, age
-     * sq.frm`person`.ins`age`.val`${12}`.ret`id, age`
+     * sq.from`person`.insert`age`.value`${12}`.return`id, age`
      * // insert into person (age) values (12) returning id, age
      * sq`person``age > ${7}``id, age`
      * // select id, age from person where age > 7
      */
-    ret(strings: TemplateStringsArray, ...args: any[]): sq
+    return(strings: TemplateStringsArray, ...args: any[]): sq
 
     /**
      * SELECT or RETURNING clause - specify columns query returns
@@ -123,92 +123,136 @@ declare module 'sqorn' {
      * Accepts columns as strings
      * 
      * @example
-     * sq.ret('1', '2', '3')
+     * sq.return('1', '2', '3')
      * // select 1, 2, 3
-     * sq.frm('book').ret('title')
+     * sq.from('book').return('title')
      * // select title from book
-     * sq.del.frm('person').ret('id', 'age')
+     * sq.delete.from('person').return('id', 'age')
      * // delete from person returning id, age
-     * sq.frm('person').set`age = age + 1`.ret('id', 'age')
+     * sq.from('person').set`age = age + 1`.return('id', 'age')
      * // update person set age = age + 1 returning id, age
-     * sq.frm('person').ins('age').val(12).ret('id', 'age')
+     * sq.from('person').insert('age').value(12).return('id', 'age')
      * // insert into person (age) values (12) returning id, age
      * sq`person``age > ${7}`('id', 'age')
      * // select id, age from person where age > 7
      */
-    ret(...columns: string[]): sq
+    return(...columns: string[]): sq
 
     /**
      * INSERT column - specify columns to insert using tagged template literal
      * 
-     * The query must also include at least one call to`sq.val` specifing the value(s) to insert
+     * The query must also include at least one call to`.value` specifing the
+     * values to insert as tagged template literals
      * 
      * @example
-     * sq.frm`person`.ins`first_name, last_name`.val`'Carmen', 'San Diego'`
-     * // insert into person (first_name, last_name) values ('Carment', 'San Diego')
-     * sq.frm`person`.ins`age`.val`23`.val`40`.ret`id`
+     * sq.from`person`.insert`first_name, last_name`.value`'Jo', 'Jo'`
+     * // insert into person (first_name, last_name) values ('Jo', 'Jo')
+     * sq.from`person`.insert`age`.value`${23}`.value`${40}`.return`id`
      * // insert into person (age) values (23), (40) returning id
-     * sq`person`()`id`.ins`age`.val`23`.val`40`
+     * sq`person````id`.insert`age`.value`23`.value`40`
      * // insert into person (age) values (23), (40) returning id
      */
-    ins(strings: TemplateStringsArray, ...args: any[]): sq
+    insert(strings: TemplateStringsArray, ...args: any[]): sq
+
     /**
-     * INSERT value - specify value(s) to insert as objects
+     * INSERT column - specify columns to insert as strings
+     * 
+     * The query must also include at least one call to`.value` specifing the
+     * values to insert as function arguments
      * 
      * @example
-     * sq.frm`person`.ins({ firstName: 'Bob' })
+     * sq.from('book').insert('title', 'published').value('1984', 1949)
+     * // insert into book (title, published) values ('1984', 1949)
+     * sq.from('person').insert('name', 'age').value('Jo', 9).value(null)
+     * // insert into person (name, age) values ('Jo', 9), (null, default)
+     * sq`person`()`id`.insert('age').value('23')
+     * // insert into person (age) values (23), (40) returning id
+     */
+    insert(...columns: string[]): sq
+
+    /**
+     * INSERT value - specify rows to insert as objects
+     * 
+     * Each object passed to `.insert` represents a row to insert. Column names
+     * are inferred from object keys. `null` values are converted to SQL `null`
+     * while `undefined` values are converted to SQL `default`
+     * 
+     * @example
+     * sq.from`person`.insert({ firstName: 'Bob' })
      * // insert into person (first_name) values ('Bob')
-     * sq.frm`person`.ins({ firstName: 'Bob' }, { lastName: 'Baker' })
+     * sq.from`person`.insert({ firstName: 'Bob' }, { lastName: 'Baker' })
      * // insert into person (first_name, last_name) values ('Bob', default), (default, 'Baker')
-     * sq`person`()`id`.ins({ firstName: 'Bob' }
+     * sq`person`.insert({ name: 'Bob' }).insert({ name: null, age: 7 })
+     * // insert into person (name, age) values ('Bob', default), (null, 7)
+     * sq`person`()`id`.insert({ firstName: 'Bob' }
      * // insert into person (first_name) values ('Bob') returning id
      */
-    ins(...values: object[]): sq
+    insert(...values: { [string]: any }[]): sq
 
-        /**
+    /**
      * VALUE - specify values to insert as tagged template literals
      * 
-     * The query must also include a call to`sq.ins` specifing the columns to insert into
+     * The query must also include a call to`sq.insert` specifing columns
      * 
      * @example
-     * sq.frm`person`.ins`first_name, last_name`.val`'Carmen', 'San Diego'`
-     * // insert into person (first_name, last_name) values ('Carment', 'San Diego')
-     * sq.frm`person`.ins`age`.val`23`.val`40`.ret`id`
+     * sq.from`person`.insert`first_name, last_name`.value`'Jo', 'Jo'`
+     * // insert into person (first_name, last_name) values ('Jo', 'Jo')
+     * sq.from`person`.insert`age`.value`${23}`.value`${40}`.return`id`
      * // insert into person (age) values (23), (40) returning id
-     * sq`person`()`id`.ins`age`.val`23`.val`40`
+     * sq`person````id`.insert`age`.value`23`.value`40`
      * // insert into person (age) values (23), (40) returning id
      */
-    val(strings: TemplateStringsArray, ...args: any[]): sq
+    value(strings: TemplateStringsArray, ...args: any[]): sq
+
+    /**
+     * VALUE - specify values to insert as function arguments
+     * 
+     * The query must also include a call to`sq.insert` specifing columns
+     * 
+     * @example
+     * sq.from('book').insert('title', 'published').value('1984', 1949)
+     * // insert into book (title, published) values ('1984', 1949)
+     * sq.from('person').insert('name', 'age').value('Jo', 9).value(null)
+     * // insert into person (name, age) values ('Jo', 9), (null, default)
+     * sq`person`()`id`.insert('age').value('23')
+     * // insert into person (age) values (23), (40) returning id
+     */
+    value(...args: any[]): sq
 
     /**
      * DELETE - marks the query as a delete query
      * 
      * @example
-     * sq.del.frm`person`
+     * sq.delete.from`person`
      * // delete * from person
-     * sq.del.frm`person`.whr`age < 7`.ret`id`
+     * sq.delete.from`person`.where`age < 7`.return`id`
      * // delete from person where age < 7 returning id
-     * sq`person``age < 7``id`.del
+     * sq`person``age < 7``id`.delete
      * // delete from person where age < 7 returning id
      */
-    readonly del: sq
+    readonly delete: sq
 
     /**
      * Compiles the query buillder state, returning the equivalent SQL query string
      * 
      * @example
-     * sq.ret`1, 2, 3`.str === 'select 1, 2, 3'
-     * sq.frm`person`.str === 'select * from person'
+     * sq.return`1, 2, 3`.str === 'select 1, 2, 3'
+     * sq.from`person`.str === 'select * from person'
      */
     readonly str: string
   }
 
-  /**
-   * creates and returns a query builder with the given configuration
-   * 
-   */
-  function configure(): sq
+  interface Config {
+    pg?: any
+  }
 
-  export = configure
+  /**
+   * Creates and returns a query builder with the given configuration
+   * 
+   * @param {any} [config.pg] - node-postgres connection configuration
+   */
+  function sqorn(config?: Config): sq
+
+  export = sqorn
 }
 
