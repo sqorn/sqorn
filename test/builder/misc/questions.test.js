e3485638a57d5d72549ec8832b86dfc6d6192a4d
eejdoowad <eejdoowad@gmail.com>
2018-09-17 15:15:01 -0400

    add manual query answer to issue #9

--- test/builder/misc/questions.test.js
+++ test/builder/misc/questions.test.js
@@ -18,15 +18,39 @@ describe('tutorial', () => {
     ]
     const storeId = 12
     const scheduleId = 23
-    const q = sq.from({ t: 'trad.bcas' }).from({ v: bcas })
-      .set`quantity = v.quantity::integer`.where({
-      't.storeId': storeId,
-      't.scheduleId': scheduleId,
-      join: sq.l`t.product_code = v.code`
-    }).return`*`
     query({
       name: 'solution',
-      query: q,
+      query: sq.from({ t: 'trad.bcas' }).from({ v: bcas })
+        .set`quantity = v.quantity::integer`.where({
+        't.storeId': storeId,
+        't.scheduleId': scheduleId,
+        join: sq.l`t.product_code = v.code`
+      }).return`*`,
+      text:
+        'update trad.bcas as t set quantity = v.quantity::integer from (values ($1, $2), ($3, $4), ($5, $6)) as v(quantity, code) where (t.store_id = $7 and t.schedule_id = $8 and t.product_code = v.code) returning *',
+      args: [1, '2310', 2, '2730', 3, '3511', 12, 23]
+    })
+    query({
+      name: 'solution - insert query',
+      query: sq.from({ t: 'trad.bcas' }).from({ v: bcas })
+        .set`quantity = v.quantity::integer`.where({
+        't.storeId': storeId,
+        't.scheduleId': scheduleId,
+        join: sq.l`t.product_code = v.code`
+      }).return`*`,
+      text:
+        'update trad.bcas as t set quantity = v.quantity::integer from (values ($1, $2), ($3, $4), ($5, $6)) as v(quantity, code) where (t.store_id = $7 and t.schedule_id = $8 and t.product_code = v.code) returning *',
+      args: [1, '2310', 2, '2730', 3, '3511', 12, 23]
+    })
+
+    const values = sq.extend(...bcas.map(b => sq.l`(${b.quantity}, ${b.code})`))
+      .join`, `
+    query({
+      name: 'solution - manual query',
+      query: sq.l`update trad.bcas as t`.l`set quantity = v.quantity::integer`
+        .l`from (values ${values}) as v(quantity, code)`
+        .l`where (t.store_id = ${storeId} and t.schedule_id = ${scheduleId} and t.product_code = v.code)`
+        .l`returning *`,
       text:
         'update trad.bcas as t set quantity = v.quantity::integer from (values ($1, $2), ($3, $4), ($5, $6)) as v(quantity, code) where (t.store_id = $7 and t.schedule_id = $8 and t.product_code = v.code) returning *',
       args: [1, '2310', 2, '2730', 3, '3511', 12, 23]
