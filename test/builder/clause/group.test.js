610db1bb1e7f1e057ec879086458b954166e1949
eejdoowad <eejdoowad@gmail.com>
2018-10-03 20:23:55 -0500

    add group by docs and tests, restructure sqorn-core

--- test/builder/clause/group.test.js
+++ test/builder/clause/group.test.js
@@ -1,22 +1,185 @@
 const { sq, query } = require('../tape')
 
 describe('Group By', () => {
-  query({
-    name: 'simple',
-    query: sq.from`person`.group`age`,
-    text: 'select * from person group by age',
-    args: []
+  describe('template string', () => {
+    query({
+      name: 'simple',
+      query: sq.from`person`.group`age`,
+      text: 'select * from person group by age',
+      args: []
+    })
+    query({
+      name: 'two calls',
+      query: sq.from`person`.group`age`.group`last_name`,
+      text: 'select * from person group by age, last_name',
+      args: []
+    })
+    query({
+      name: 'three calls',
+      query: sq.from`person`.group`age`.group`last_name`.group`first_name`,
+      text: 'select * from person group by age, last_name, first_name',
+      args: []
+    })
   })
-  query({
-    name: 'two calls',
-    query: sq.from`person`.group`age`.group`last_name`,
-    text: 'select * from person group by age, last_name',
-    args: []
+  describe('expression', () => {
+    query({
+      name: 'string',
+      query: sq.from`person`.group('age'),
+      text: 'select * from person group by age',
+      args: []
+    })
+    query({
+      name: '3 strings',
+      query: sq.from`person`.group('age', 'last_name', 'first_name'),
+      text: 'select * from person group by age, last_name, first_name',
+      args: []
+    })
+    query({
+      name: 'subquery',
+      query: sq.from`person`.group(sq.l`age`),
+      text: 'select * from person group by age',
+      args: []
+    })
+    query({
+      name: 'multiple subqueries',
+      query: sq.from`person`.group(
+        sq.l`age`,
+        sq.l`last_name`,
+        sq.l`first_name`
+      ),
+      text: 'select * from person group by age, last_name, first_name',
+      args: []
+    })
+    query({
+      name: 'mixed strings and subqueries',
+      query: sq.from`person`.group(sq.l`age`, 'last_name', sq.l`first_name`),
+      text: 'select * from person group by age, last_name, first_name',
+      args: []
+    })
+    query({
+      name: 'array',
+      query: sq.from`person`.group([], ['age'], ['last_name', 'first_name']),
+      text: 'select * from person group by (), (age), (last_name, first_name)',
+      args: []
+    })
   })
-  query({
-    name: 'three calls',
-    query: sq.from`person`.group`age`.group`last_name`.group`first_name`,
-    text: 'select * from person group by age, last_name, first_name',
-    args: []
+  describe('postgres rollup', () => {
+    const { rollup } = sq
+    query({
+      name: 'rollup one arg',
+      query: sq.from`person`.group(rollup('age')),
+      text: 'select * from person group by rollup (age)',
+      args: []
+    })
+    query({
+      name: 'rollup three args',
+      query: sq.from`person`.group(rollup('age', 'last_name', 'first_name')),
+      text: 'select * from person group by rollup (age, last_name, first_name)',
+      args: []
+    })
+    query({
+      name: 'expressions and rollup args',
+      query: sq.from`person`.group('age', rollup('last_name'), 'first_name'),
+      text: 'select * from person group by age, rollup (last_name), first_name',
+      args: []
+    })
+    query({
+      name: 'rollup array arg',
+      query: sq.from`person`.group(
+        rollup([], ['age'], ['last_name', 'first_name'])
+      ),
+      text: 'select * from person group by rollup (age)',
+      args: []
+    })
+  })
+  describe('postgres cube', () => {
+    const { cube } = sq
+    query({
+      name: 'cube one arg',
+      query: sq.from`person`.group(cube('age')),
+      text: 'select * from person group by cube (age)',
+      args: []
+    })
+    query({
+      name: 'cube three args',
+      query: sq.from`person`.group(cube('age', 'last_name', 'first_name')),
+      text: 'select * from person group by cube (age, last_name, first_name)',
+      args: []
+    })
+    query({
+      name: 'expressions and cube args',
+      query: sq.from`person`.group('age', cube('last_name'), 'first_name'),
+      text: 'select * from person group by age, cube (last_name), first_name',
+      args: []
+    })
+    query({
+      name: 'cube array arg',
+      query: sq.from`person`.group(
+        cube([], ['age'], ['last_name', 'first_name'])
+      ),
+      text: 'select * from person group by cube (age)',
+      args: []
+    })
+  })
+
+  describe('postgres grouping sets', () => {
+    const { groupingSets } = sq
+    query({
+      name: 'groupingSets one arg',
+      query: sq.from`person`.group(groupingSets('age')),
+      text: 'select * from person group by grouping sets (age)',
+      args: []
+    })
+    query({
+      name: 'groupingSets three args',
+      query: sq.from`person`.group(
+        groupingSets('age', 'last_name', 'first_name')
+      ),
+      text:
+        'select * from person group by grouping sets (age, last_name, first_name)',
+      args: []
+    })
+    query({
+      name: 'expressions and groupingSets args',
+      query: sq.from`person`.group(
+        'age',
+        groupingSets('last_name'),
+        'first_name'
+      ),
+      text:
+        'select * from person group by age, grouping sets (last_name), first_name',
+      args: []
+    })
+    query({
+      name: 'groupingSets array arg',
+      query: sq.from`person`.group(
+        groupingSets([], ['age'], ['last_name', 'first_name'])
+      ),
+      text: 'select * from person group by grouping sets (age)',
+      args: []
+    })
+    query({
+      name: 'groupingSets of groupingSets',
+      query: sq.from`person`.group(
+        groupingSets(
+          groupingSets([]),
+          groupingSets(['age']),
+          groupingSets(['last_name', 'first_name'])
+        )
+      ),
+      text:
+        'select * from person group by grouping sets (grouping sets (()), grouping sets ((age)), grouping sets ((first_name, last_name)))',
+      args: []
+    })
+  })
+  describe('complex', () => {
+    query({
+      name: 'multiple mixed calls',
+      query: sq.from`person`.group(sq.l`age`).group`last_name`.group(
+        sq.l`first_name`
+      ),
+      text: 'select * from person group by age, last_name, first_name',
+      args: []
+    })
   })
 })
